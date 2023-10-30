44bf1cee74a8c2783ca456b230cdc39e8aadce0d
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-10-30 15:12:08 -0400

    Update view_script.js

--- Project03/view_script.js
+++ Project03/view_script.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  //Disabling the button unless there is something typed
+  document.querySelector('#submit').disabled = true;
+  document.querySelector('#name').onkeyup = () => {
+      if(document.querySelector('#name').value.length > 0 )
+        document.querySelector('#submit').disabled = false;
+      else
+        document.querySelector('#submit').disabled = true;
+  };
 
