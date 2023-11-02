39c3328b1dcae3baf09b217366a94d2cd4d8b739
nikkitarrao <144383426+nikkitarrao@users.noreply.github.com>
2023-11-02 16:38:37 -0400

    Update view_script.js

--- Project03/view_script.js
+++ Project03/view_script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+  
   //Disabling the button unless there is something typed
   document.querySelector('#submit').disabled = true;
   document.querySelector('#name').onkeyup = () => {
@@ -7,4 +8,25 @@ document.addEventListener('DOMContentLoaded', function() {
       else
         document.querySelector('#submit').disabled = true;
   };
+
+//Saving the entered name
+  var name = document.querySelector('#name').value;
+  
+//Asynchronous Network Request
+async function backEndRestAPI(){
+  const response = await fetch('https://www.ixl.com/math/grade-4/identify-place-value-names');
+  const data = await response.json;
+  updateDom(data);
+}
+  
+//Updating DOM  
+function updateDom(){
+ // let question = 
+}
+
+
+
+
+
+  
 });
